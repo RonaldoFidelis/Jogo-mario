a67692588080b8ee61eb05eff8c7a0134d0cb6ff
RonaldoFidelis <ronaldofidelis.ti@gmail.com>
2022-11-13 19:26:48 -0300

    playable

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const personagem = document.querySelector('.personagem');
-
+const fireBall = document.querySelector('.fire-ball')
 const jump = () => {
     personagem.classList.add('jump');
 
@@ -8,4 +8,22 @@ const jump = () => {
     }, 500)
 };
 
+const loop = setInterval(() => {
+    const fireBallPosition = fireBall.offsetLeft;
+    const personagemPositon = +window.getComputedStyle(personagem).bottom.replace('px', ' ')
+
+    if(fireBallPosition <= 85  && fireBallPosition > 0 && personagemPositon <=25){
+        fireBall.style.animation = 'none';
+        fireBall.style.left=`${fireBallPosition}px`;
+
+        personagem.style.animation = 'none';
+        personagem.style.bottom = `${personagemPositon}`;
+
+        personagem.src = './img/gamerOver.png';
+        personagem.style.width = '70px';
+
+        clearInterval(loop);
+    }
+
+}, 10)
 document.addEventListener('keydown', jump);
